c7af724ab963d59b9c87759bf7fe70157cfc582f
achebotarev <aleksey.chebotarev@homecredit.ru>
2017-11-10 16:24:34 +0300

    Exclude players module from backend

--- src/main/java/ru/bender/pokerstatistic/players/PlayerControllerImpl.java
+++
@@ -1,7 +0,0 @@
-package ru.bender.pokerstatistic.players;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-class PlayerControllerImpl implements PlayerController {
-}
--- src/main/java/ru/bender/pokerstatistic/players/PlayerDao.java
+++
@@ -1,6 +0,0 @@
-package ru.bender.pokerstatistic.players;
-
-import org.springframework.data.repository.CrudRepository;
-
-interface PlayerDao extends CrudRepository<Player, Integer> {
-}
